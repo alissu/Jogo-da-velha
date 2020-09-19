b1b12c9bd66ea8358a67166256fdd65e17cfb079
Alisson <alissonlucas1821@gmail.com>
2020-09-18 23:22:38 -0300

    Adicionando componentes e Elevando o estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
+}
+
+const Square: React.FC<{value: number}> = props => {
+  const [value, setValue] = React.useState<string | null>(null);
+
+  return (<button className='sqaure' onClick={() => setValue('X')}>
+    {value}
+  </button>);
+}
+
+const Board: React.FC = () => {
+  const [square, setSquare] = React.useState<null[] | number[]>(Array(9).fill(null));
+
+  const renderSquare = (value: number) => {
+    return <Square value={value} />;
+  }
+
+  const status = 'Next player X';
+  return (<div>
+    <div className='status'>{status}</div>
+    <div className='board-row'>
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
+    </div>
+    <div className='board-row'>
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+    </div>
+    <div className='board-row'>
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
+    </div>
+  </div>);
+}
+
+const Game: React.FC = () => {
+  return(<div className='game'>
+    <div>
+      <Board />
+    </div>
+    <div className='game-info'>
+      <div> {/* STATUS */} </div>
+      <div> {/* TODO */} </div>
+    </div>
+  </div>);
 }
 
 export default App;
